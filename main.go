c04c6ac8a6356cc006529a7b6a9eddb447c73ffd
Liam Middlebrook <liammiddlebrook@gmail.com>
2017-07-31 17:08:10 -0700

    eww

--- main.go
+++ main.go
@@ -1,29 +1,29 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"github.com/golang/glog"
-	"net/http"
-	"runtime"
+    "flag"
+    "fmt"
+    "github.com/golang/glog"
+    "net/http"
+    "runtime"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	glog.Infof("Received request from %s for %s", r.RemoteAddr, r.URL)
+    glog.Infof("Received request from %s for %s", r.RemoteAddr, r.URL)
     glog.Flush()
 
-	h := w.Header()
-	h.Set("Content-Type", "text/plain")
+    h := w.Header()
+    h.Set("Content-Type", "text/plain")
 
-	fmt.Fprint(w, "Hello world!\n\n")
-	fmt.Fprintf(w, "Go version: %s\n", runtime.Version())
+    fmt.Fprint(w, "Hello world!\n\n")
+    fmt.Fprintf(w, "Go version: %s\n", runtime.Version())
 }
 
 func main() {
-	flag.Parse()
+    flag.Parse()
 
-	glog.Info("Starting server...")
+    glog.Info("Starting server...")
     glog.Flush()
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+    http.HandleFunc("/", handler)
+    http.ListenAndServe(":8080", nil)
 }
