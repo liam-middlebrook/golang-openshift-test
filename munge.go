e8f1cb7bdfbbb83f39ddeb89c5d4928ee27bdaf4
Liam Middlebrook <liammiddlebrook@gmail.com>
2017-07-31 23:46:40 -0700

    runescape

--- munge.go
+++ munge.go
@@ -31,7 +31,7 @@ func main() {
 
     r := gin.Default()
 
-    r.GET('/', func (c *gin.Context) {
+    r.GET("/", func (c *gin.Context) {
         c.JSON(200, gin.H{
             "message": "MUNGE",
         })
