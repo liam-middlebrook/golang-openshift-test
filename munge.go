0d6f41bbf293d53671427565281e3f53c2dc87b2
Liam Middlebrook <liammiddlebrook@gmail.com>
2017-08-03 13:07:53 -0700

    csh_auth struct update

--- munge.go
+++ munge.go
@@ -1,34 +1,45 @@
 package main
 
 import (
-    "flag"
-    "net/http"
-    log "github.com/sirupsen/logrus"
-    "github.com/gin-gonic/gin"
-    csh_auth "github.com/liam-middlebrook/csh-auth"
+	"flag"
+	"github.com/gin-gonic/gin"
+	csh_auth "github.com/liam-middlebrook/csh-auth"
+	log "github.com/sirupsen/logrus"
+	"net/http"
+	"os"
 )
 
-func protectedProfile(c *gin.Context){
-    claims, ok := c.Value(csh_auth.AuthKey).(csh_auth.CSHClaims)
-    if !ok {
-        log.Fatal("error finding claims")
-        return
-    }
-    c.String(http.StatusOK, "uid %s email %s name %s uuid %s", claims.UserInfo.Username, claims.UserInfo.Email, claims.UserInfo.FullName, claims.UserInfo.Subject)
+func protectedProfile(c *gin.Context) {
+	claims, ok := c.Value(csh_auth.AuthKey).(csh_auth.CSHClaims)
+	if !ok {
+		log.Fatal("error finding claims")
+		return
+	}
+	c.String(http.StatusOK, "uid %s email %s name %s uuid %s", claims.UserInfo.Username, claims.UserInfo.Email, claims.UserInfo.FullName, claims.UserInfo.Subject)
 }
 
 func main() {
-    flag.Parse()
+	flag.Parse()
 
-    log.Info("Starting server...")
+	log.Info("Starting server...")
 
-    // needs to be declared here not inline so provider is global XXX FIXME
-    r := gin.Default()
-    csh_auth.Init("/auth/login")
-    r.GET("/", csh_auth.AuthWrapper(func (c *gin.Context) { c.String(http.StatusOK, "spooky data") }))
-    r.GET("/test", csh_auth.AuthWrapper(protectedProfile))
-    r.GET("/auth/login", csh_auth.AuthRequest)
-    r.GET("/auth/redir", csh_auth.AuthCallback)
-    r.GET("/auth/logout", csh_auth.AuthLogout)
-    r.Run()
+	r := gin.Default()
+	csh := csh_auth.CSHAuth{}
+	log.Info("csh auth")
+	csh.Init(
+		os.Getenv("csh_auth_client_id"),
+		os.Getenv("csh_auth_client_secret"),
+		os.Getenv("csh_auth_jwt_secret"),
+		os.Getenv("csh_auth_state"),
+		os.Getenv("csh_auth_server_host"),
+		os.Getenv("csh_auth_redirect_uri"),
+		"/auth/login",
+	)
+	log.Info("csh auth INIT")
+	r.GET("/", csh.AuthWrapper(func(c *gin.Context) { c.String(http.StatusOK, "spooky data") }))
+	r.GET("/test", csh.AuthWrapper(protectedProfile))
+	r.GET("/auth/login", csh.AuthRequest)
+	r.GET("/auth/redir", csh.AuthCallback)
+	r.GET("/auth/logout", csh.AuthLogout)
+	r.Run()
 }
